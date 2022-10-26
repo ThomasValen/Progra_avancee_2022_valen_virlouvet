9772496b75169a61037e885eb05db9080ce54e90
Virlouvet <evann.virlouvet9@etu.univ-lorraine.fr>
2022-10-26 15:35:49 +0200

    reglage de different bug

---
+++ eventsMain.h
@@ -0,0 +1,29 @@
+#ifndef __EVENTSMAIN__H__
+#define __EVENTSMAIN__H__
+#include "world.h"
+#include "graphisme.h"
+#include "sdl2-light.h"
+#include "sdl2-ttf-light.h"
+
+
+/**
+ * \brief La fonction gère les évènements ayant eu lieu et qui n'ont pas encore été traités
+ * \param event paramètre qui contient les événements
+ * \param world les données du monde
+ */
+
+void handle_events(SDL_Event *event,world_t *world);
+
+
+/**
+ * \brief fonction qui initialise le jeu: initialisation de la partie graphique (SDL), chargement des textures, initialisation des données
+ * \param window la fenêtre du jeu
+ * \param renderer le renderer
+ * \param textures les textures
+ * \param world le monde
+ */
+
+void init(SDL_Window **window, SDL_Renderer ** renderer, textures_t *textures, world_t * world); 
+
+
+#endif
--- world.h
+++ world.h
@@ -13,9 +13,22 @@ struct world_s{
     int longueur_tab;
     int hauteur_tab;
     int **tab;
-    sprite_t background;
-    sprite_t wall[1000];
+    sprite_t* background;
+    sprite_t* wall[1000];
 };
 typedef struct world_s world_t;
+
+
+void init_data(world_t * world);
+
+void init_valeurs(world_t* world);
+
+void init_environnement(world_t* world);
+
+void init_sprite(sprite_t *sprite,int x,int y,int h,int l);
+
+int** generate_world(int ligne,int colonne);
+
+int nb_murs(world_t*world);
 
 #endif
